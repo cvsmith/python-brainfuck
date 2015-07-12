1bd5c9f11a54f05a3e02663153dc7d0c5363b7c0
cvsmith <cvsmith@andrew.cmu.edu>
2015-07-12 01:48:53 -0400

    Create Brainfuck class. Populate it with methods for each brainfuck command
    Nested loops still don't work

--- brainfuck.py
+++ brainfuck.py
@@ -1,29 +1,76 @@
 # Brainfuck interpreter in python
 
-guide = dict()
-guide['>'] = 'ptr += 1'
-guide['<'] = 'ptr -= 1'
-guide['+'] = 'array[ptr] += 1'
-guide['-'] = 'array[ptr] -= 1'
-guide['.'] = 'print array[ptr]'
-guide[','] = 'array[ptr] = int(raw_input(">"))'
-guide['['] = ('if array[ptr] == 0:\n'
-              '    while code[i] != "]":\n'
-              '        i += 1')
-guide[']'] = ('if array[ptr] != 0:\n'
-              '    while code[i] != "[":\n'
-              '        i -= 1')
+class Brainfuck:
 
-array = [0 for i in xrange(8)]
-array[0] = 1
-ptr = 0
+    def __init__(self):
+        self.guide = self.make_guide()
+    
+    def make_guide(self):
+        guide = dict()
 
-code = ">>[-]<<[->>+<<]" # Moves value at current cell two cells right
+        guide['>'] = self.inc_ptr
+        guide['<'] = self.dec_ptr
+        guide['+'] = self.inc_byte
+        guide['-'] = self.dec_byte
+        guide['.'] = self.output
+        guide[','] = self.input
+        guide['['] = self.start_loop
+        guide[']'] = self.end_loop
 
-i = 0
-while i < len(code):
-    cmd = code[i]
-    exec(guide[cmd])
-    i += 1
+        return guide
 
-print array
+    def inc_ptr(self):
+        if self.arr_ptr == len(self.arr) - 1:
+            self.arr.append(0)
+        self.arr_ptr += 1
+
+    def dec_ptr(self):
+        if self.arr_ptr == 0:
+            self.arr.insert(0, 0)
+        else:
+            self.arr_ptr -= 1
+
+    def inc_byte(self):
+        self.arr[self.arr_ptr] += 1
+
+    def dec_byte(self):
+        self.arr[self.arr_ptr] -= 1
+
+    def output(self):
+        print self.arr[self.arr_ptr]
+
+    def input(self):
+        self.arr[self.arr_ptr] = int(raw_input(">"))
+
+    # TODO fix nested loop failure
+    def start_loop(self):
+        if self.arr[self.arr_ptr] == 0:
+            while self.code[self.cmd_ptr] != "]":
+                self.cmd_ptr += 1
+
+    def end_loop(self):
+        if self.arr[self.arr_ptr] != 0:
+            while self.code[self.cmd_ptr] != "[":
+                self.cmd_ptr -= 1
+
+    def run(self, code, trace=False):
+        self.arr = [0]
+        self.arr_ptr = 0
+        self.cmd_ptr = 0
+        self.code = code
+        while self.cmd_ptr < len(self.code):
+            cmd = self.code[self.cmd_ptr]
+            if trace: print self.arr, self.arr_ptr, cmd
+            self.guide[cmd]()
+            self.cmd_ptr += 1
+
+        return self.arr
+
+brainfuck = Brainfuck()
+
+# Takes input, moves it two cells right
+print brainfuck.run(",>>[-]<<[->>+<<]", trace=True)
+
+# Hello world
+print brainfuck.run("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.",
+                    trace=True)
